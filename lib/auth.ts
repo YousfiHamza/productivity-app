753859642902d553e55f59726657326091fb0951
YoufiHamza <yousfihamza22@gmail.com>
2023-03-30 06:08:59 +0800

    password hash

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,6 @@
-export const hashPassword = () => {
-  return "hashPassword";
-};
+import bcrypt from 'bcrypt';
+
+export const hashPassword = (password: string) => bcrypt.hash(password, 10);
+
+export const comparePasswords = (plainTextPassword: string, hashedPassword: string) =>
+  bcrypt.compare(plainTextPassword, hashedPassword);
